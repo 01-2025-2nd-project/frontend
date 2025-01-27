7828452afd7b43f818f55d4431c9bac903abf9a2
flsgk <harinjung.az@gmail.com>
2025-01-27 14:01:08 +0900

    style : 로그인 페이지 UI 수정
    - 카카오톡 로그인 버튼 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -32,45 +32,62 @@ const SubmitButton = styled.button`
   padding: 0.5rem;
 `;
 
+const SocialButton = styled.button`
+  background: #04b404;
+  border: none;
+  border-radius: 10px;
+  padding: 0.5rem;
+  width: 20rem;
+`;
+
 const Typo = styled.span`
   font-size: 0.5em;
+`;
+
+const Divider = styled.hr`
+  border-top: 0.05em solid #fafafa;
+  width: 20rem;
 `;
 
 export default function LoginPage() {
   const { formData, errors, handleInputChange, handleLogin } = useLogin();
 
   return (
-    <Form name="loginForm">
-      <InputContainer>
-        <Label>이메일</Label>
-        <InputBox
-          onChange={handleInputChange}
-          name="email"
-          value={formData.email}
-          type="text"
-          placeholder="이메일을 입력하세요"
-        ></InputBox>
-        <br />
-        <Typo>{errors.email}</Typo>
-      </InputContainer>
+    <>
+      <Form name="loginForm">
+        <SocialButton type="button">카카오톡으로 로그인하기</SocialButton>
+        <Divider />
+        <InputContainer>
+          <Label>이메일</Label>
+          <InputBox
+            onChange={handleInputChange}
+            name="email"
+            value={formData.email}
+            type="text"
+            placeholder="이메일을 입력하세요"
+          ></InputBox>
+          <br />
+          <Typo>{errors.email}</Typo>
+        </InputContainer>
 
-      <InputContainer>
-        <Label>비밀번호</Label>
-        <InputBox
-          onChange={handleInputChange}
-          name="password"
-          value={formData.password}
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-        ></InputBox>
-        <br />
-        <Typo>{errors.password}</Typo>
-      </InputContainer>
+        <InputContainer>
+          <Label>비밀번호</Label>
+          <InputBox
+            onChange={handleInputChange}
+            name="password"
+            value={formData.password}
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+          ></InputBox>
+          <br />
+          <Typo>{errors.password}</Typo>
+        </InputContainer>
 
-      <Typo>{errors.allField}</Typo>
-      <SubmitButton type="button" onClick={handleLogin}>
-        로그인하기
-      </SubmitButton>
-    </Form>
+        <Typo>{errors.allField}</Typo>
+        <SubmitButton type="button" onClick={handleLogin}>
+          로그인하기
+        </SubmitButton>
+      </Form>
+    </>
   );
 }
