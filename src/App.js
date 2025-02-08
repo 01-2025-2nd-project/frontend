3034461e99bd9c5765c41452ba4c0ba9e64c1474
ines012 <celeste07102@gmail.com>
2025-02-09 07:44:58 +0800

    fix: params productId로 수정

--- src/App.js
+++ src/App.js
@@ -11,13 +11,12 @@ import store from "./redux/store.js";
 
 function App() {
   return (
-
     <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/product/:productId/party" element={<TotalParties />} />
-          <Route path="/product/:product_id" element={<ProductDetail />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/my" element={<MyPage />} />
@@ -25,7 +24,6 @@ function App() {
         </Routes>
       </Router>
     </Provider>
-
   );
 }
 
